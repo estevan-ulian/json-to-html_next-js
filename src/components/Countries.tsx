574146d76d0025a3883e271b823056e4e1e65fae
estevan-ulian <estevan.ulian@gmail.com>
2022-07-07 07:49:46 -0300

    React Icons install

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,5 +1,6 @@
 import Ul from "./Ul"
 import { useFetch } from '../hooks/useFetch'
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
 type Country = {
   name: string
@@ -13,7 +14,12 @@ const Countries = () => {
 
     return (
       <Ul>
-      {isFetching && <p>Carregando!!</p>}
+      {isFetching && (
+        <div className="flex items-center justify-center h-80">
+          <AiOutlineLoading3Quarters size={40} color='#4c91f8' className="animate-loading" />
+        </div>
+        
+      )}
       {countries?.map((data, i) => {
         return(
           <li className='border-b pb-1 mb-1 border-gray-200' key={i}>{data.name_ptbr} | {data.code}</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        loading: {
+          '0%' : {transform: 'rotate(0deg)'},
+          '100%': {transform: 'rotate(360deg)'}
+        }
+      },
+      animation: {
+        loading: 'loading 2s infinite'
+      }
+    },
   },
   plugins: [],
 }
